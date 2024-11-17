da6cb01fa7fa292f4cac3028b5274bc6fac8169a
chaouche-fouad <104238307+chaouche-fouad@users.noreply.github.com>
2024-11-17 03:36:41 +0100

    Update main.py

--- Spelling Checker/main.py
+++ Spelling Checker/main.py
@@ -15,7 +15,7 @@ def check_spelling():
 
     cs = Label(root, text=f"Correct text is : \"{right}\"" ,font=("poppins",20), bg="#dae6f6", fg="#364971")
     cs.place(x=100, y=250)
-    # spell.config(text=right)
+    
 
 heading = Label(root,text="Spelling Checker", font=("Trebuchet Ms", 30, "bold"), bg="#dae6f6", fg="#364971")
 heading.pack(pady=(50,0))
@@ -27,6 +27,5 @@ enter_text.focus()
 button = Button(root, text="Check", font=("Arial", 20, "bold"),fg="white", bg="red", command=check_spelling)
 button.pack()
 
-# spell = Label(root, font=("poppins", 20),bg="#dae6f6",fg="#364971")
-# spell.place(x=350,y=250)
+
 root.mainloop()
